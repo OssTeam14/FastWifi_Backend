ff415547f932a43c71efe881e72b7036edb6104b
Jansoon <ckdals4600@naver.com>
2023-11-16 01:45:34 +0900

    ✨feat: Add User Entity

---
+++ src/main/java/oss/fastwifi/user/entity/User.java
@@ -0,0 +1,76 @@
+package oss.fastwifi.user.entity;
+
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Collection;
+import java.util.HashSet;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue
+    @Column(name = "user_id")
+    private Long id;
+
+    private String uid;
+
+    private String password;
+
+    private String name;
+
+    @ColumnDefault("False")
+    private Boolean isAuth;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return new HashSet<GrantedAuthority>();
+    }
+
+    @Override
+    public String getUsername() {
+        return id.toString();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+
+    @Builder
+    public User(String uid, String password, String name){
+        this.uid = uid;
+        this.password = password;
+        this.name = name;
+    }
+
+
+}
