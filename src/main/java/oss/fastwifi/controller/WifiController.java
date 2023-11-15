eeac8887c92a87e7e448d208fcd38bf8d828e841
jansoon <ckdals4600@naver.com>
2023-11-16 00:29:13 +0900

    🎨 feat: 인증 없이 와이파이 정보 조회

---
+++ src/main/java/oss/fastwifi/controller/WifiController.java
@@ -0,0 +1,59 @@
+package oss.fastwifi.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import oss.fastwifi.dto.response.WifiForListDTO;
+import oss.fastwifi.dto.response.WifiWithoutPwdDTO;
+import oss.fastwifi.response.Response;
+import oss.fastwifi.response.StatusEnum;
+import oss.fastwifi.service.WifiService;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@AllArgsConstructor
+@RequestMapping("/wifi")
+public class WifiController {
+
+    private final WifiService wifiService;
+
+    @GetMapping("/{building}/{floor}")
+    public ResponseEntity<Response> buildingAndFloor(
+            @PathVariable String building,
+            @PathVariable int floor
+    ){
+        List<WifiForListDTO> wifiList = wifiService.getWifiListWithoutPwd(building, floor);
+
+        Response body = Response.builder()
+                .status(StatusEnum.OK)
+                .data(wifiList)
+                .message(building + floor + "층 와이파이 정보")
+                .build();
+
+        return new ResponseEntity<>(body,Response.getDefaultHeader(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{building}/{floor}/{wifiName}")
+    public ResponseEntity<Response> wifiInfoWithoutPwd(
+            @PathVariable String building,
+            @PathVariable int floor,
+            @PathVariable String wifiName
+    ){
+        WifiWithoutPwdDTO wifiList = wifiService.getWifiInfoWithoutPwd(building, floor, wifiName);
+
+        Response body = Response.builder()
+                .status(StatusEnum.OK)
+                .data(wifiList)
+                .message(building + floor + "층 " + wifiName + " 와이파이 정보")
+                .build();
+
+        return new ResponseEntity<>(body,Response.getDefaultHeader(), HttpStatus.OK);
+    }
+}
