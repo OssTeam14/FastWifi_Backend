b23880af5bb97ae92484985bbe965de8cd745665
Jansoon <ckdals4600@naver.com>
2023-11-17 10:58:44 +0900

    ✨ Feat: Jwt 요소 추가

---
+++ src/main/java/oss/jwt/CustomJwtException.java
@@ -0,0 +1,12 @@
+package oss.jwt;
+
+import lombok.Getter;
+import oss.fastwifi.error.dto.ErrorCode;
+import oss.fastwifi.error.exception.BusinessException;
+
+@Getter
+public class CustomJwtException extends BusinessException {
+    public CustomJwtException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+}
