834acd49b373081c13225ff0e5cffbf4cf14eae6
Jansoon <ckdals4600@naver.com>
2023-11-18 02:24:00 +0900

    🚑 Fix: Change Integer type annotation form @NotBlank to @NotNull

--- src/main/java/oss/fastwifi/wifi/dto/request/WifiReq.java
+++ src/main/java/oss/fastwifi/wifi/dto/request/WifiReq.java
@@ -3,13 +3,14 @@ package oss.fastwifi.wifi.dto.request;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
 public class WifiReq {
     @NotBlank(message = "건물 이름이 필요합니다.")
     private String buildingName;
 
-    @NotBlank(message = "층 정보가 필요합니다.")
+    @NotNull(message = "층 정보가 필요합니다.")
     private Integer floor;
 
     @NotBlank(message = "와이파이 이름이 필요합니다")
