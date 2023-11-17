3181dd307ca7e1815f0bad409ab63eddf791d713
Jansoon <ckdals4600@naver.com>
2023-11-18 02:24:51 +0900

    ✨ Feat: 이메일 정규식 추가

--- src/main/java/oss/fastwifi/common/Constants.java
+++ src/main/java/oss/fastwifi/common/Constants.java
@@ -8,7 +8,7 @@ public final class Constants {
 
     public static final String ID_REGEXP = "^(?=.*[a-zA-z])(?=.*[0-9]).{6,15}$";
     public static final String PW_REGEXP = "^(?=.*[a-zA-z])(?=.*[0-9]).{8,16}$";
-    public static final String PHONE_NUM_REGEXP = "^01(?:0|1|[6-9])(?:\\d{3}|\\d{4})\\d{4}$";
+    public static final String MAIL_REGEXP = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$";
 
     public static final String RANDOM_UID_BASE_STR = "fastWifi";
     public static final int RANDOM_UID_CODE_LENGTH = 4;
