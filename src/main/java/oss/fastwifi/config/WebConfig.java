8177f784e551df0fe2b476bdb97a65060ecd0c4f
Jansoon <ckdals4600@naver.com>
2023-11-18 02:27:19 +0900

    ✨ Feat: WebConfig 추가

---
+++ src/main/java/oss/fastwifi/config/WebConfig.java
@@ -0,0 +1,23 @@
+package oss.fastwifi.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Configuration
+public class WebConfig{
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+
+}
