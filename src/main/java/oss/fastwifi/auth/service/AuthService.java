ae552fca0d6afff691fdf5a43fb650473e7caa12
Jansoon <ckdals4600@naver.com>
2023-11-17 11:02:15 +0900

    ✨ Feat: 인증 서비스 구현

---
+++ src/main/java/oss/fastwifi/auth/service/AuthService.java
@@ -0,0 +1,120 @@
+package oss.fastwifi.auth.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import oss.fastwifi.auth.dto.request.*;
+import oss.fastwifi.auth.dto.response.AccessTokenRes;
+import oss.fastwifi.auth.dto.response.FindIdRes;
+import oss.fastwifi.auth.dto.response.TokenRes;
+import oss.fastwifi.auth.repository.RefreshTokenRedisRepository;
+import oss.fastwifi.error.dto.ErrorCode;
+import oss.fastwifi.error.exception.BusinessException;
+import oss.fastwifi.user.entity.User;
+import oss.fastwifi.user.repository.UserRepository;
+import oss.fastwifi.verification.dto.VerifyingType;
+import oss.fastwifi.verification.service.VerificationService;
+import oss.jwt.JwtTokenProvider;
+import oss.jwt.RefreshToken;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class AuthService {
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final RefreshTokenRedisRepository refreshTokenRedisRepository;
+    private final VerificationService verificationService;
+
+    @Transactional
+    public void signUp(SignUpReq signUpReq) {
+        verificationService.validateIsVerified(signUpReq.getPhoneNum(), VerifyingType.SIGN_UP);
+
+        if(!checkUidAvailable(signUpReq.getUid())) {
+            throw new BusinessException(ErrorCode.ID_ALREADY_EXISTS);
+        }
+
+        userRepository.save(User.builder()
+                .uid(signUpReq.getUid())
+                .password(passwordEncoder.encode(signUpReq.getPassword()))
+                .phoneNum(signUpReq.getPhoneNum())
+                .name(signUpReq.getName())
+                .build());
+    }
+
+    public TokenRes login(LoginReq loginReq) {
+        User user = userRepository.findByUid(loginReq.getUid()).orElseThrow(() -> new BusinessException(ErrorCode.WRONG_ID));
+        if (!passwordEncoder.matches(loginReq.getPassword(), user.getPassword())) {
+            throw new BusinessException(ErrorCode.WRONG_PW);
+        }
+        TokenRes token = TokenRes.builder()
+                .accessToken(jwtTokenProvider.createAccessToken(user.getId()))
+                .refreshToken(jwtTokenProvider.createRefreshToken(user.getId()))
+                .build();
+        return token;
+    }
+
+    public boolean checkUidAvailable(String uid) {
+        return !userRepository.existsByUid(uid);
+    }
+
+    public AccessTokenRes refresh(RefreshReq refreshReq) {
+        String requestToken = refreshReq.getRefreshToken();
+
+        jwtTokenProvider.validateRefreshToken(requestToken);
+
+        RefreshToken redisToken = refreshTokenRedisRepository.findById(requestToken).orElseThrow(() -> new BusinessException(ErrorCode.INVALID_JWT_TOKEN));
+        if ((redisToken.getUserId() != Long.parseLong(jwtTokenProvider.getUserId(requestToken)))) {
+            throw new BusinessException(ErrorCode.INVALID_JWT_TOKEN);
+        }
+
+        AccessTokenRes newAccessToken = AccessTokenRes.builder()
+                .accessToken(jwtTokenProvider.createAccessToken(redisToken.getUserId()))
+                .build();
+        return newAccessToken;
+    }
+
+    public void logout(Long userId) {
+        RefreshToken token = refreshTokenRedisRepository.findByUserId(userId);
+        refreshTokenRedisRepository.deleteById(token.getRefreshToken());
+    }
+
+    public FindIdRes findId(FindIdReq findIdReq) {
+        String phoneNum = findIdReq.getPhoneNum();
+        String name = findIdReq.getName();
+
+        verificationService.validateIsVerified(phoneNum, VerifyingType.FIND_ID);
+
+        List<User> users = userRepository.findAllByNameAndPhoneNum(name,phoneNum);
+        List<String> uids = users.stream().map(User::getUid).collect(Collectors.toList());
+
+        return FindIdRes.builder()
+                .uids(uids)
+                .build();
+    }
+
+    @Transactional
+    public void changePw(ChangePwReq changePwReq) {
+        String phoneNum = changePwReq.getPhoneNum();
+        String uid = changePwReq.getUid();
+        String newPassword = changePwReq.getNewPassword();
+        String confirmPassword = changePwReq.getConfirmPassword();
+
+        verificationService.validateIsVerified(phoneNum, VerifyingType.FIND_PW);
+        validateNewPassword(newPassword,confirmPassword);
+
+        User user = userRepository.findByUid(uid).orElseThrow(() -> new BusinessException(ErrorCode.NOT_FOUND));
+        user.updatePassword(passwordEncoder.encode(newPassword));
+    }
+
+    private void validateNewPassword(String newPassword, String confirmPassword) {
+        if (!newPassword.equals(confirmPassword)) {
+            throw new BusinessException(ErrorCode.UNMATCHING_NEW_PASSWORD);
+        }
+    }
+}
