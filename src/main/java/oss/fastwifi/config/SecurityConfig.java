762398e61f275d396abf76175d24a1ed044468a7
jansoon <ckdals4600@naver.com>
2023-11-21 14:45:30 +0900

    Fix: Cors 오류 해결

--- src/main/java/oss/fastwifi/config/SecurityConfig.java
+++ src/main/java/oss/fastwifi/config/SecurityConfig.java
@@ -37,6 +37,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .cors().and()
                 .csrf().disable()
                 .httpBasic().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
