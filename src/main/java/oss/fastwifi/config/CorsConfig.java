4e453ba29d238ab62eb2bd240eed847c69e4f084
Jansoon <ckdals4600@naver.com>
2023-11-21 17:41:41 +0900

    Fix: Cors 에러 해결 진행 중

--- src/main/java/oss/fastwifi/config/CorsConfig.java
+++ src/main/java/oss/fastwifi/config/CorsConfig.java
@@ -2,15 +2,20 @@ package oss.fastwifi.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
+@Configuration
+@EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+                .allowedOriginPatterns("*")
                 .allowedMethods("*")
-                .allowedOrigins("http://localhost:8080");
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
